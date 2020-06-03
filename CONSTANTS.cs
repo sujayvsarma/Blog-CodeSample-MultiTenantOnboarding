25d35a632daf1303700b88c441192d7c245447e2
Sujay Sarma <sujay@sarma.in>
2020-06-03 16:16:06 +0530

    Initial commit

---
+++ CONSTANTS.cs
@@ -0,0 +1,57 @@
+﻿namespace MultiTenantSample
+{
+
+    public static class CONSTANTS
+    {
+
+        public static string SHARE_NAME = "mtsample";
+
+        public static string GLOBAL_ADMINISTRATOR_ROLE_ID = "62e90394-69f5-4237-9190-012177145e10";
+
+        public static string GRAPHAPI_SCOPE_DEFAULT = "https://graph.microsoft.com/.default";
+
+        /// <summary>
+        /// Default scopes for Graph API calls
+        /// </summary>
+        public static string[] GRAPHAPI_SCOPES_LIST = new string[] {
+            "https://graph.microsoft.com/User.Read",
+            "https://graph.microsoft.com/User.Read.All",
+            "https://graph.microsoft.com/User.ReadBasic.All",
+            "https://graph.microsoft.com/Group.Read.All",
+            "https://graph.microsoft.com/Group.ReadWrite.All"
+        };
+
+
+        public static string[] DEFAULT_AUTH_SCOPES = new string[]
+        {
+            "https://graph.microsoft.com/User.Read"
+        };
+
+        /// <summary>
+        /// Default scopes for AzureRM calls
+        /// </summary>
+        public static string[] AZURERM_SCOPE = new string[] { "https://management.core.windows.net/user_impersonation" };
+
+        /// <summary>
+        /// Scopes for admin consent presented during onboarding workflow
+        /// </summary>
+        public static string[] ONBOARDING_ADMIN_CONSENT_SCOPES_LIST = new string[]
+        {
+            "https://graph.microsoft.com/Directory.AccessAsUser.All",
+            "https://graph.microsoft.com/Directory.Read.All",            
+            "https://graph.microsoft.com/Group.Read.All",
+            "https://graph.microsoft.com/Group.ReadWrite.All",
+            "https://graph.microsoft.com/Organization.Read.All",
+            "https://graph.microsoft.com/User.Read.All",
+            "https://management.core.windows.net/user_impersonation"
+        };
+
+
+        /// <summary>
+        /// The wellknown tenant Id for the AzureAD "common" & "consumers" endpoint
+        /// </summary>
+        public static System.Guid AZUREAD_TENANTID_WELLKNOWN_COMMON = System.Guid.Parse("9188040d-6c67-4c5b-b112-36a304b66dad");
+    }
+
+
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiTenantSample.Controllers
+{
+
+    /// <summary>
+    /// This is our home controller. This controller does nothing useful in this 
+    /// sample except to show up the default homepage at app startup.
+    /// </summary>
+    [AllowAnonymous, Controller]
+    public class HomeController : Controller
+    {
+
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View("Homepage");
+        }
+
+    }
+}
